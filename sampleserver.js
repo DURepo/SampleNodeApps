083e8efae2cad6e614372121a8ad62e08ac3e3cb
DURepo <doshna.u@gmail.com>
2020-06-10 16:31:35 -0700

    Added express app

--- sampleserver.js
+++ sampleserver.js
@@ -1,5 +1,7 @@
 var http = require('http');
 
+console.log('server is up');
+
 function onRequest(request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
     response.write('Hello World');
